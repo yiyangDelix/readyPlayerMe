51b0828b77d10887f204d8e4b0cf491dc71a81d9
yiyangDelix <73201079+yiyangDelix@users.noreply.github.com>
2026-02-21 20:02:29 +0100

    Create PatientState.cs

---
+++ PatientState.cs
@@ -0,0 +1,11 @@
+class PatientState
+{
+    public float anxiety;
+    public string personality;
+    public string diagnosis;
+    public string symptomProfile;
+
+
+
+    GeneratePrompt(PatientState state);
+}
